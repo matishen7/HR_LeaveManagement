419442b0114a788290ac02b46b9e005cd1ce9668
Muratbek Ishenbaev <matishen7@gmail.com>
2023-09-28 13:09:45 -0700

    LeaveType

---
+++ HR_LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace HR_LeaveManagement.Domain
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public int DefaultDays { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
