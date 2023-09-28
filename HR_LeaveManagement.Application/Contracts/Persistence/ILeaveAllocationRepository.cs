951c7defbd109422795c2c4bf8ad54fabd6aa5b0
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-09-28 14:11:36 -0700

    ILeaveAllocationRepository

---
+++ HR_LeaveManagement.Application/Contracts/Persistence/ILeaveAllocationRepository.cs
@@ -0,0 +1,9 @@
+﻿using HR_LeaveManagement.Domain;
+
+namespace HR_LeaveManagement.Application.Contracts.Persistence
+{
+    public interface ILeaveAllocationRepository : IGenericRepository<LeaveAllocation>
+    {
+
+    }
+}
