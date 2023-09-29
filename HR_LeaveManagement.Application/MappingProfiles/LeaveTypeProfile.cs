808c0c34c9738ee263da0f5a81ef940066b1a3c1
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-09-29 15:17:44 -0700

    LeaveTypeProfile

---
+++ HR_LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HR_LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.MappingProfiles
+{
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile()
+        {
+            CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+        }
+    }
+}
