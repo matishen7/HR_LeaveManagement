69bafac011ae3df49ad88ef06eecfe0f3eecb5d9
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-04 15:09:30 -0700

    GetLeaveTypeQuery

---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypeQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+    //public class GetLeaveTypeQuery : IRequest<List<LeaveTypeDto>>
+    //{
+    //}
+
+    public record GetLeaveTypeQuery : IRequest<List<LeaveTypeDto>>
+
+}
