abdcc6cf7c413498dbb5ea3f0e05f036bc93249d
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-05 07:26:37 -0700

    LeaveTypeDetailsDto

---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveType/Queries/GetLeaveTypeDetails/LeaveTypeDetailsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveType.Queries.GetLeaveTypeDetails
+{
+    public class LeaveTypeDetailsDto
+    {
+        public int DefaultDays { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime? DateCreated { get; set; }
+        public int Id { get; set; }
+        public DateTime? DateModified { get; set; }
+    }
+}
