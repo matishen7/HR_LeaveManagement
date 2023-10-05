6daf24d0d244bd3a7a8fd0d73ba477a097cc56b4
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-05 08:24:39 -0700

    CreateLeaveTypeCommandHandler

--- HR_LeaveManagement.Application/Contracts/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -1,17 +1,32 @@
-﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using HR_LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
 
 namespace HR_LeaveManagement.Application.Contracts.Features.LeaveType.Commands.CreateLeaveType
 {
     public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
     {
-        public Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        private readonly IMapper mapper;
+        private readonly ILeaveTypeRepository leaveTypeRepository;
+
+        public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
         {
-            throw new NotImplementedException();
+            this.mapper = mapper;
+            this.leaveTypeRepository = leaveTypeRepository;
+        }
+        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            // validate request
+            // convert  request to domain object
+
+            var leaveTypeToCreate = mapper.Map<Domain.LeaveType>(request);
+
+            // add to db
+            await leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+
+            // return id
+
+            return leaveTypeToCreate.Id;
         }
     }
 }
