0f283df47fa995614aae21d9177e5b111916c735
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-05 08:32:18 -0700

    UpdateLeaveTypeCommandHandler

---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using HR_LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveType.Commands.UpdateLeaveType
+{
+    public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
+    {
+        private readonly IMapper mapper;
+        private readonly ILeaveTypeRepository leaveTypeRepository;
+
+        public UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            this.mapper = mapper;
+            this.leaveTypeRepository = leaveTypeRepository;
+        }
+        public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            // validate request
+            // convert  request to domain object
+
+            var leaveTypeToCreate = mapper.Map<Domain.LeaveType>(request);
+
+            // add to db
+            await leaveTypeRepository.UpdateAsync(leaveTypeToCreate);
+
+            // return id
+
+            return Unit.Value;
+        }
+    }
+}
