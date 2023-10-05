d402f764b092d2646a86e7b7dc29e9029bd80fed
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-05 09:55:05 -0700

    BadRequestException

---
+++ HR_LeaveManagement.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,10 @@
+﻿namespace HR_LeaveManagement.Application.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+
+        }
+    }
+}
