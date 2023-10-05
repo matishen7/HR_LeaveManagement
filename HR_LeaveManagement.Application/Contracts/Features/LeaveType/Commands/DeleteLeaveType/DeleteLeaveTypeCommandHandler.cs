f357cb70799b8e60bcdc1014e9e8ca5492985316
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-05 08:40:05 -0700

    DeleteLeaveTypeCommandHandler

---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using HR_LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveType.Commands.DeleteLeaveType
+{
+    public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+    {
+        private readonly ILeaveTypeRepository leaveTypeRepository;
+
+        public DeleteLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            this.leaveTypeRepository = leaveTypeRepository;
+        }
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+
+            // get from db
+            var leaveTypeToDelete = await leaveTypeRepository.GetByIdAsync(request.Id);
+
+            //validate record exists
+
+            // delete from db
+            await leaveTypeRepository.DeleteAsync(leaveTypeToDelete);
+
+            // return id
+
+            return Unit.Value;
+        }
+    }
+}
