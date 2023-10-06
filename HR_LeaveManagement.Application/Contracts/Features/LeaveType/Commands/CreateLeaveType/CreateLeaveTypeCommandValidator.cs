3eb35281d9d92828e90a51258590b4bc85e4d301
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-06 13:07:04 -0700

    CreateLeaveTypeCommandValidator

---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandValidator : AbstractValidator<CreateLeaveTypeCommand>
+    {
+        public CreateLeaveTypeCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(70).WithMessage("{PropertyName} must be fewer than 70 characters.");
+        }
+
+    }
+}
