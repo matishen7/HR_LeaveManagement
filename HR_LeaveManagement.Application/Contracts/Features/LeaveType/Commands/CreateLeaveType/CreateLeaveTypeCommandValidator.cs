3970b792d1660080fb2789ef43eaf3ec801bd9b5
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-06 13:33:38 -0700

    IsLeaveTypeUnique

--- HR_LeaveManagement.Application/Contracts/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandValidator.cs
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandValidator.cs
@@ -1,10 +1,13 @@
 ﻿using FluentValidation;
+using HR_LeaveManagement.Application.Contracts.Persistence;
 
 namespace HR_LeaveManagement.Application.Contracts.Features.LeaveType.Commands.CreateLeaveType
 {
     public class CreateLeaveTypeCommandValidator : AbstractValidator<CreateLeaveTypeCommand>
     {
-        public CreateLeaveTypeCommandValidator()
+        private readonly ILeaveTypeRepository leaveTypeRepository;
+
+        public CreateLeaveTypeCommandValidator(ILeaveTypeRepository leaveTypeRepository)
         {
             RuleFor(p => p.Name)
                 .NotEmpty().WithMessage("{PropertyName} is required.")
@@ -14,7 +17,17 @@ namespace HR_LeaveManagement.Application.Contracts.Features.LeaveType.Commands.C
             RuleFor(p => p.DefaultDays)
                 .GreaterThan(100).WithMessage("{PropertyName} cannot exceed 100")
                 .LessThan(1).WithMessage("{PropertyName} cannot be less than 1");
+
+            RuleFor(q => q)
+                .MustAsync(LeaveTypeNameUnique)
+                .WithMessage("Leave Type name must be unique.");
+
+            this.leaveTypeRepository = leaveTypeRepository;
         }
 
+        private Task<bool> LeaveTypeNameUnique(CreateLeaveTypeCommand command, CancellationToken token)
+        {
+            return leaveTypeRepository.IsLeaveTypeUnique(command.Name);
+        }
     }
 }
--- HR_LeaveManagement.Application/Contracts/Persistence/ILeaveTypeRepository.cs
+++ HR_LeaveManagement.Application/Contracts/Persistence/ILeaveTypeRepository.cs
@@ -4,6 +4,6 @@ namespace HR_LeaveManagement.Application.Contracts.Persistence
 {
     public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
     {
-
+        Task<bool> IsLeaveTypeUnique(string name);
     }
 }
