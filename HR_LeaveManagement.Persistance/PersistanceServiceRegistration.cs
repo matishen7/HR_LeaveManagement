5a2b39c57d8a91fcc796db9fc0b7acb6b6040fc8
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-09 12:27:12 -0700

    AddPersistenceServices

--- HR_LeaveManagement.Persistance/PersistanceServiceRegistration.cs
+++ HR_LeaveManagement.Persistance/PersistanceServiceRegistration.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using HR_LeaveManagement.Persistance.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HR_LeaveManagement.Persistance
 {
     public static class PersistanceServiceRegistration
     {
-        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<HrDatabaseContext>(options => { 
+                options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
+            });
             return services;
         }
     }
