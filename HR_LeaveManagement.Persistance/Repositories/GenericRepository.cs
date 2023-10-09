d07803880bb5267ee1ab1403649812a13c1ecb67
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-09 12:31:53 -0700

    GenericRepository

---
+++ HR_LeaveManagement.Persistance/Repositories/GenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using HR_LeaveManagement.Application.Contracts.Persistence;
+
+namespace HR_LeaveManagement.Persistance.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+
+    {
+        public Task<T> CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
