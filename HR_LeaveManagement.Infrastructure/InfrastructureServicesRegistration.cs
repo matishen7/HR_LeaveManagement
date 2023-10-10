b31ee1f617b73aa87a0dbecfbb98e5c713c977cf
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-10 10:37:56 -0700

    InfrastructureServicesRegistration

---
+++ HR_LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR_LeaveManagement.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
