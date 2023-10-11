d311fe8c2010380499d3e47cf3320f1482fa7dd3
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-11 13:59:16 -0700

    IEmailSender

---
+++ HR_LeaveManagement.Application/Contracts/Email/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Contracts.Email
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
