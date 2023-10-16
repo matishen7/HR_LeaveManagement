c163317deed2076b5519f545330afa0bd264f5bc
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-16 13:07:45 -0700

    Task<List<LeaveTypeDto>> Get

---
+++ HR_LeaveManagement.Api/Controllers/LeaveTypeController.cs
@@ -0,0 +1,52 @@
+﻿using HR_LeaveManagement.Application.Contracts.Features.LeaveType.Queries.GetAllLeaveTypes;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HR_LeaveManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveTypeController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public LeaveTypeController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        // GET: api/<LeaveTypeController>
+        [HttpGet]
+        public async Task<List<LeaveTypeDto>> Get()
+        {
+            return await mediator.Send(new GetLeaveTypeQuery());
+        }
+
+        // GET api/<LeaveTypeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LeaveTypeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LeaveTypeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LeaveTypeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
