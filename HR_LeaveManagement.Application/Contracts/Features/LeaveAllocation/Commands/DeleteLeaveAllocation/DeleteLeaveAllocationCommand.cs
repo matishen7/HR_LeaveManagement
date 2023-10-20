128295469f1f0ef408b7b65f38c188f65e2ea4e8
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-20 12:30:32 -0700

    DeleteLeaveAllocationCommand

---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveAllocation/Commands/DeleteLeaveAllocation/DeleteLeaveAllocationCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveAllocation.Commands.DeleteLeaveAllocation
+{
+    public class DeleteLeaveAllocationCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
