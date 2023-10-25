ea1ff06bddf58eb10140a3088f6bd14138a5af88
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-25 13:43:01 -0700

    GetLeaveRequestListQuery

---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveRequests/Queries/GetLeaveRequestListQuery.cs
@@ -0,0 +1,14 @@
+﻿using HR_LeaveManagement.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveRequests.Queries
+{
+    public class GetLeaveRequestListQuery : IRequest<LeaveRequestDto>
+    {
+    }
+}
---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveRequests/Queries/LeaveRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using HR_LeaveManagement.Application.Contracts.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveRequests.Queries
+{
+    public class LeaveRequestDto
+    {
+        public LeaveTypeDto? LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public DateTime? DateRequested { get; set; }
+        public string? RequestComments { get; set; }
+        public bool Approved { get; set; }
+        public bool Cancelled { get; set; }
+        public string RequestingEmployeeId { get; set; } = string.Empty;
+    }
+}
