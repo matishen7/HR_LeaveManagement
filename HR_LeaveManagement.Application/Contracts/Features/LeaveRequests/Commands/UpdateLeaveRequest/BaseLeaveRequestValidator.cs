642266d8e964b769142162912acb69ecffea95aa
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-27 12:15:51 -0700

    BaseLeaveRequestValidator

---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveRequests/Commands/UpdateLeaveRequest/BaseLeaveRequestValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using HR_LeaveManagement.Application.Contracts.Persistence;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveRequests.Commands.UpdateLeaveRequest
+{
+    public class BaseLeaveRequestValidator : AbstractValidator<BaseLeaveRequest>
+    {
+        private readonly ILeaveTypeRepository leaveTypeRepository;
+
+        public BaseLeaveRequestValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            RuleFor(p => p.StartDate)
+                .LessThan(p => p.EndDate).WithMessage("{PropertyName} must be before {ComparisonValue}.");
+
+            RuleFor(p => p.EndDate)
+                .GreaterThan(p => p.StartDate).WithMessage("{PropertyName} must be after {ComparisonValue}.");
+
+            RuleFor(q => q.LeaveTypeId)
+                .MustAsync(LeaveTypeMustExist)
+                .WithMessage("Leave Type must exist");
+
+            this.leaveTypeRepository = leaveTypeRepository;
+        }
+
+        private async Task<bool> LeaveTypeMustExist(int id, CancellationToken token)
+        {
+            var leaveType = await leaveTypeRepository.GetByIdAsync(id);
+            return leaveType != null;
+        }
+
+    }
+}
