6090c5014bc056d536d47d6760fec381256a16ab
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-30 08:40:15 -0700

    CancelLeaveRequestCommand

---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveRequests/Commands/CancelLeaveRequest/CancelLeaveRequestCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveRequests.Commands.CancelLeaveRequest
+{
+    public class CancelLeaveRequestCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
