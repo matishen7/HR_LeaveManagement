987e0fc2a2c4ea14640200b6ddaec42be51bcd6b
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-30 09:16:52 -0700

    ChangeLeaveRequestCommand

---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveRequests/Commands/ChangeLeaveRequest/ChangeLeaveRequestCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveRequests.Commands.ChangeLeaveRequest
+{
+    public class ChangeLeaveRequestCommand : IRequest
+    {
+        public int Id { get; set; }
+        public bool Approved { get; set; }
+    }
+}
