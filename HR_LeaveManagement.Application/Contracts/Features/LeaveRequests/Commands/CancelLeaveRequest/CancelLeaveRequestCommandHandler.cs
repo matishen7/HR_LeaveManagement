d871282d5958609cf70ab3e116df336b39ac6270
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-30 08:43:16 -0700

    CancelLeaveRequestCommandHandler

---
+++ HR_LeaveManagement.Application/Contracts/Features/LeaveRequests/Commands/CancelLeaveRequest/CancelLeaveRequestCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using HR_LeaveManagement.Application.Contracts.Features.LeaveRequests.Commands.DeleteLeaveRequest;
+using HR_LeaveManagement.Application.Contracts.Persistence;
+using HR_LeaveManagement.Application.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Contracts.Features.LeaveRequests.Commands.CancelLeaveRequest
+{
+    public class CancelLeaveRequestCommandHandler : IRequestHandler<CancelLeaveRequestCommand, Unit>
+    {
+        private readonly ILeaveRequestRepository leaveRequestRepository;
+
+        public CancelLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository)
+        {
+            this.leaveRequestRepository = leaveRequestRepository;
+        }
+        public async Task<Unit> Handle(CancelLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            // query database
+            var leaveRequest = await leaveRequestRepository.GetLeaveRequestWithDetails(request.Id);
+            if (leaveRequest == null)
+            {
+                throw new NotFoundException(nameof(leaveRequest), request.Id);
+            }
+            leaveRequest.Cancelled = true;
+            await leaveRequestRepository.UpdateAsync(leaveRequest);
+
+            return Unit.Value;
+
+
+        }
+    }
+}
