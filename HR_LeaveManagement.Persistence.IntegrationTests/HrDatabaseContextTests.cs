24e6a74ce090fcfea4f67108d4e178d1cc7e3ef4
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-31 13:24:58 -0700

    Save_SetDateMOdifiedValue

--- HR_LeaveManagement.Persistence.IntegrationTests/HrDatabaseContextTests.cs
+++ HR_LeaveManagement.Persistence.IntegrationTests/HrDatabaseContextTests.cs
@@ -1,5 +1,7 @@
+using HR_LeaveManagement.Domain;
 using HR_LeaveManagement.Persistance.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
+using Shouldly;
 
 namespace HR_LeaveManagement.Persistence.IntegrationTests
 {
@@ -14,9 +16,43 @@ namespace HR_LeaveManagement.Persistence.IntegrationTests
             _hrDbCOntext = new HrDatabaseContext(_dbOptions);
         }
         [Fact]
-        public void Save()
+        public async Task Save_SetDateCreatedValueAsync()
         {
+            //Arrange
+            var leaveType = new LeaveType
+            {
+                Id = 1,
+                DefaultDays = 1,
+                Name = "Sick",
+            };
 
+            //Act
+
+            await _hrDbCOntext.LeaveTypes.AddAsync(leaveType);
+            await _hrDbCOntext.SaveChangesAsync();
+            //Assert
+
+            leaveType.DateCreated.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async void Save_SetDateMOdifiedValue()
+        {
+            //Arrange
+            var leaveType = new LeaveType
+            {
+                Id = 1,
+                DefaultDays = 1,
+                Name = "Sick",
+            };
+
+            //Act
+
+            await _hrDbCOntext.LeaveTypes.AddAsync(leaveType);
+            await _hrDbCOntext.SaveChangesAsync();
+            //Assert
+
+            leaveType.DateModified.ShouldNotBeNull();
         }
     }
 }
