381574ef56de6a1b78782bb0baccdf54c4bacc55
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-10-31 08:48:42 -0700

    MockLeaveTypeRepository

---
+++ HR_LeaveManagement.Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,44 @@
+﻿using HR_LeaveManagement.Application.Contracts.Persistence;
+using HR_LeaveManagement.Domain;
+using Moq;
+
+namespace HR_LeaveManagement.Application.UnitTests.Mocks
+{
+    public class MockLeaveTypeRepository
+    {
+        public static Mock<ILeaveTypeRepository> GetMockLeaveTypeRepository()
+        {
+            var leaveTypes = new List<LeaveType>()
+            {
+                new LeaveType
+                {
+                    Id = 1,
+                    DefaultDays = 1,
+                    Name = "Sick",
+                },
+                new LeaveType
+                {
+                    Id = 2,
+                    DefaultDays = 10,
+                    Name = "Paternity",
+                },
+                new LeaveType
+                {
+                    Id = 3,
+                    DefaultDays = 15,
+                    Name = "Maternity",
+                }
+            };
+
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+            mockRepo.Setup(r => r.GetAsync()).ReturnsAsync(leaveTypes);
+            mockRepo.Setup(r => r.CreateAsync(It.IsAny<LeaveType>()))
+                .Returns((LeaveType leaveType) =>
+                {
+                    leaveTypes.Add(leaveType);
+                    return Task.CompletedTask;
+                });
+            return mockRepo;
+        }
+    }
+}
