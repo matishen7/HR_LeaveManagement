02757886bd18b438ea16c12787b5ddbd3c5b573a
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-11-07 08:25:08 -0800

    GetLeaveAllocationListHandlerTests

--- HR_LeaveManagement.Application.UnitTests/Features/LeaveAllocations/Queries/GetLeaveAllocationListHandlerTests.cs
+++ HR_LeaveManagement.Application.UnitTests/Features/LeaveAllocations/Queries/GetLeaveAllocationListHandlerTests.cs
@@ -1,16 +1,11 @@
 ﻿using AutoMapper;
 using HR_LeaveManagement.Application.Contracts.Features.LeaveAllocation.Queries.GetAllLeaveAllocation;
-using HR_LeaveManagement.Application.Contracts.Features.LeaveType.Queries.GetLeaveTypeDetails;
 using HR_LeaveManagement.Application.Contracts.Logging;
 using HR_LeaveManagement.Application.Contracts.Persistence;
 using HR_LeaveManagement.Application.MappingProfiles;
 using HR_LeaveManagement.Application.UnitTests.Mocks;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Shouldly;
 
 namespace HR_LeaveManagement.Application.UnitTests.Features.LeaveAllocations.Queries
 {
@@ -20,9 +15,9 @@ namespace HR_LeaveManagement.Application.UnitTests.Features.LeaveAllocations.Que
         private readonly IMapper _mapper;
         private readonly Mock<IAppLogger<GetLeaveAllocationListHandler>> _mockAppLogger;
 
-        public GetLeaveTypeDetailsHandlerTests()
+        public GetLeaveAllocationListHandlerTests()
         {
-            _mockRepo = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+            _mockRepo = MockLeaveAllocationRepository.GetMockLeaveAllocationRepository();
             var mapperConfig = new MapperConfiguration(c =>
             {
                 c.AddProfile<LeaveAllocationProfile>();
@@ -35,10 +30,10 @@ namespace HR_LeaveManagement.Application.UnitTests.Features.LeaveAllocations.Que
         [Fact]
         public async Task GetLeaveAllocationList_Success()
         {
-            var handler = new GetLeaveTypeDetailsHandler(_mapper, _mockRepo.Object, _mockAppLogger.Object);
-            var result = await handler.Handle(new GetLeaveTypeDetailsQuery(1), CancellationToken.None);
-            result.ShouldBeOfType<LeaveTypeDetailsDto>();
-            result.Id.ShouldBe(1);
+            var handler = new GetLeaveAllocationListHandler(_mapper, _mockRepo.Object);
+            var result = await handler.Handle(new GetLeaveAllocationListQuery(), CancellationToken.None);
+            result.ShouldBeOfType<LeaveAllocationDto>();
+            result.Count.ShouldBe(3);
         }
     }
 }
