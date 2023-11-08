50637a126c34f19de93015c21d3f8f78163f52ae
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-11-08 11:29:45 -0800

    add interface IClient

---
+++ HR_LeaveManagement.BlazorUI/Services/Base/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace HR_LeaveManagement.BlazorUI.Services.Base
+{
+    public partial class Client : IClient
+    {
+        public HttpClient HttpClient
+        {
+            get
+            {
+                return _httpClient;
+            }
+        }
+    }
+}
