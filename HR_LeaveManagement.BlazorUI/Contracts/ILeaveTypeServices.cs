7d1a25cbf4924277655d79ac6ddff0ac01722cf4
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-11-09 14:00:26 -0800

    ILeaveTypeServices

--- HR_LeaveManagement.BlazorUI/Contracts/ILeaveTypeServices.cs
+++ HR_LeaveManagement.BlazorUI/Contracts/ILeaveTypeServices.cs
@@ -1,4 +1,5 @@
 ﻿using HR_LeaveManagement.BlazorUI.Models;
+using HR_LeaveManagement.BlazorUI.Services.Base;
 
 namespace HR_LeaveManagement.BlazorUI.Contracts
 {
@@ -6,5 +7,8 @@ namespace HR_LeaveManagement.BlazorUI.Contracts
     {
         Task<List<LeaveTypeVM>> GetLeaveTypes();
         Task<LeaveTypeVM> GetLeaveTypeDetails(int id);
+        Task<Response<Guid>> CreateLeaveType(LeaveTypeVM vm);
+        Task<Response<Guid>> UpdateLeaveType(int id, LeaveTypeVM vm);
+        Task<Response<Guid>> DeleteLeaveType(int id);
     }
 }
