89f1e9d500e44f1b83fdf7e6207aca1c3873e0e8
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-11-09 13:56:27 -0800

    LeaveTypeVM

---
+++ HR_LeaveManagement.BlazorUI/Models/LeaveTypeVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR_LeaveManagement.BlazorUI.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Default number of days")]
+        public int  DefaultDays { get; set; }
+
+    }
+}
