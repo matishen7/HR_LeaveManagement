3f1a2cf95beeac68a97f4224c0cc2ce600c3cd73
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-11-22 11:44:51 -0800

    leave type index pages

---
+++ HR_LeaveManagement.BlazorUI/Pages/LeaveTypes/Index.razor.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Components;
+using HR_LeaveManagement.BlazorUI.Contracts;
+using HR_LeaveManagement.BlazorUI.Models;
+
+namespace HR_LeaveManagement.BlazorUI.Pages.LeaveTypes
+{
+    public partial class Index
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public ILeaveTypeServices LeaveTypeService { get; set; }
+
+        public List<LeaveTypeVM> LeaveTypes { get; private set; }
+        public string Message { get; set; } = string.Empty;
+
+        protected void CreateLeaveType()
+        {
+            NavigationManager.NavigateTo("/leavetypes/create/");
+        }
+
+        protected void AllocateLeaveType(int id)
+        {
+            // Use Leave Allocation Service here
+        }
+
+        protected void EditLeaveType(int id)
+        {
+            NavigationManager.NavigateTo($"/leavetypes/edit/{id}");
+        }
+
+        protected void DetailsLeaveType(int id)
+        {
+            NavigationManager.NavigateTo($"/leavetypes/details/{id}");
+        }
+
+        protected async Task DeleteLeaveType(int id)
+        {
+            var response = await LeaveTypeService.DeleteLeaveType(id);
+            if (response.Success)
+            {
+                StateHasChanged();
+            }
+            else
+            {
+                Message = response.Message;
+            }
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            LeaveTypes = await LeaveTypeService.GetLeaveTypes();
+        }
+    }
+}
