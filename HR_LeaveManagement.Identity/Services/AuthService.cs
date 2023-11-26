685e1a76a1eb98677ce8600dd532ef76cdaf912d
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-11-26 13:26:17 -0800

    AuthService

---
+++ HR_LeaveManagement.Identity/Services/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using HR_LeaveManagement.Application.Contracts.Identity;
+using HR_LeaveManagement.Application.Contracts.Models.Identity;
+using HR_LeaveManagement.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Identity.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthService(UserManager<ApplicationUser> userManager,
+            IOptions<JwtSettings> jwtSettings,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _jwtSettings = jwtSettings.Value;
+            _signInManager = signInManager;
+        }
+        public Task<AuthResponse> Login(AuthRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RegistrationResponse> Register(RegistrationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
