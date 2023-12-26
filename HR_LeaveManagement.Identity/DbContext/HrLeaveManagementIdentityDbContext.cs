00054237c85517126917520d2ad6e8e239efbe83
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-12-26 09:42:11 -0800

    Add identity roles config

---
+++ HR_LeaveManagement.Identity/DbContext/HrLeaveManagementIdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HR_LeaveManagement.Identity.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace HR_LeaveManagement.Identity.DbContext
+{
+    public class HrLeaveManagementIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public HrLeaveManagementIdentityDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<HrLeaveManagementIdentityDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(HrLeaveManagementIdentityDbContext).Assembly);
+        }
+    }
+}
